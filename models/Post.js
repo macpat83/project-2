9c1c674ac4bcbcc47aa6505d4b9694906777e174
macpat83 <macpat@gmail.com>
2022-06-22 18:46:53 -0500

    added favorites

--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 // create our Post model
 class Post extends Model {
   static upvote(body, models) {
-    return models.Vote.create({
+    return models.Favorite.create({
       user_id: body.user_id,
       post_id: body.post_id
     }).then(() => {
@@ -18,8 +18,8 @@ class Post extends Model {
           'album_art',
           'created_at',
           [
-            sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-            'vote_count'
+            sequelize.literal('(SELECT COUNT(*) FROM favorite WHERE post.id = favorite.post_id)'),
+            'favorite_count'
           ]
         ]
       });
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Post = require("./Post");
+const Favorite = require('./Favorite');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -9,4 +10,32 @@ User.hasMany(Post, {
     foreignKey: 'user_id'
   });
 
-module.exports = { User, Post };
+  User.belongsToMany(Post, {
+    through: Favorite,
+    as: 'favorited_posts',
+    foreignKey: 'user_id'
+  });
+
+  Post.belongsToMany(User, {
+    through: Favorite,
+    as: 'favorited_posts',
+    foreignKey: 'post_id'
+  });
+
+  Favorite.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Favorite.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Favorite, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Favorite, {
+    foreignKey: 'post_id'
+  });
+
+module.exports = { User, Post, Favorite};
