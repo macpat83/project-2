c3f0082d913d1da3b475906190ddecdbbddd3882
macpat83 <macpat@gmail.com>
2022-06-22 14:58:46 -0500

    added album post model/routes

--- models/index.js
+++ models/index.js
@@ -1,3 +1,12 @@
-const User = require('./User');
+const User = require("./User");
+const Post = require("./Post");
 
-module.exports = { User };
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+module.exports = { User, Post };
