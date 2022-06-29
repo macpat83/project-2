14cbe24f3c4e8681997e195913ea33c537a23fd9
Shelby Monroe <38024477+smon4544@users.noreply.github.com>
2022-06-28 21:05:35 -0500

    configure tailwind

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('public/stylesheets', express.static('public/stylesheets'));
+
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
